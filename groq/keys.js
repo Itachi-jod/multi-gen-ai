61b98577913c9ccf2d15e2cbd1e66e12de9db245
Itachi <itachijodx72@gmail.com>
2025-12-03 15:27:28 +0545

    Create keys.js

---
+++ groq/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apiKeys: [
+    "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",
+    "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",
+    "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"
+  ]
+};
