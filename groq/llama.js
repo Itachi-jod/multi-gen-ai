ff4c88316a87ef1930ea87bb103b7388254acbb7
MinatoCode <minatoofficials08@gmail.com>
2025-12-04 00:18:10 +0530

    Update llama.js

--- groq/llama.js
+++ groq/llama.js
@@ -4,12 +4,12 @@ const { apiKeys } = require("./keys");
 let keyIndex = 0;
 
 function getClient() {
-  const client = new Groq({ apiKey: apiKeys[keyIndex] });
+  const client = new Groq({
+    apiKey: apiKeys[keyIndex]
+  });
 
   keyIndex = (keyIndex + 1) % apiKeys.length;
   return client;
 }
 
-module.exports = {
-  getClient
-};
+module.exports = { getClient };
