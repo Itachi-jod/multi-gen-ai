6144ecf49c094cf3c87c5b47fd8a4b15ab42cab3
Itachi <itachijodx72@gmail.com>
2025-12-03 15:25:38 +0545

    Create genSong.js

---
+++ commands/genSong.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+module.exports = async function genSong(query) {
+  try {
+    const url = `https://minato-ytdl.vercel.app/music?q=${encodeURIComponent(query)}`;
+    const res = await axios.get(url);
+
+    return res.data?.url || null; 
+  } catch (err) {
+    return null;
+  }
+};
