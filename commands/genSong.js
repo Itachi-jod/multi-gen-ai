e7859caacf4acca331bba9643dedebb28b08631e
MinatoCode <minatoofficials08@gmail.com>
2025-12-04 00:29:24 +0530

    Update genSong.js

--- commands/genSong.js
+++ commands/genSong.js
@@ -5,7 +5,7 @@ module.exports = async function genSong(query) {
     const url = `https://minato-ytdl.vercel.app/music?q=${encodeURIComponent(query)}`;
     const res = await axios.get(url);
 
-    return res.data?.url || null; 
+    return res.data?.download_url || null; 
   } catch (err) {
     return null;
   }
