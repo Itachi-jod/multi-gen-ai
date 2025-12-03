705cbd5315fd1e0d0d37339ebc02c61ebf3977db
Itachi <itachijodx72@gmail.com>
2025-12-03 15:27:52 +0545

    Create llama.js

---
+++ groq/llama.js
@@ -0,0 +1,15 @@
+const Groq = require("groq-sdk");
+const { apiKeys } = require("./keys");
+
+let keyIndex = 0;
+
+function getClient() {
+  const client = new Groq({ apiKey: apiKeys[keyIndex] });
+
+  keyIndex = (keyIndex + 1) % apiKeys.length;
+  return client;
+}
+
+module.exports = {
+  getClient
+};
